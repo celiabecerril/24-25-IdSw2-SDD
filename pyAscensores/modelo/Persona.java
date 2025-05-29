6dffc9a6a2508e4767bdb747bf0aefecdf2ca80b
Celia Becerril Riaño <144801825+celiabecerril@users.noreply.github.com>
2025-05-29 19:20:10 +0200

    Persona controla llamada

--- pyAscensores/modelo/Persona.java
+++ pyAscensores/modelo/Persona.java
@@ -1,6 +1,7 @@
 package modelo;
 
 import java.util.Random;
+import controlador.ControlAscensor;
 
 public class Persona {
     public static final int MIN_TIEMPO_ESTANCIA = 5;
@@ -8,63 +9,86 @@ public class Persona {
 
     private static final int PLANTA_MIN = Universidad.MIN_PISO;
     private static final int PLANTA_MAX = Universidad.MAX_PISO;
-    private static final int PLANTA_INGRESO = Universidad.INGRESO;
+    private static final double PROBABILIDAD_LLAMAR = 0.8;
+    private static final Random random = new Random();
 
-    private int destino;
-    private boolean quiereSalir;
-    private int tiempoRestante;
+    private int plantaDestino;
+    private int tiempoEstancia;
+    private boolean salirDefinitivo = false;
+    private boolean haSalido = false;
 
     public Persona() {
-        this.destino = generarDestinoAleatorio();
-        this.quiereSalir = false;
-        this.tiempoRestante = MIN_TIEMPO_ESTANCIA +
-                (int) (Math.random() * (MAX_TIEMPO_ESTANCIA - MIN_TIEMPO_ESTANCIA + 1));
+        // Al crearse, cada persona genera su planta destino y tiempo de estancia aleatoriamente
+        this.plantaDestino = generarDestinoAleatorio();
+        this.tiempoEstancia = generarTiempoEstanciaAleatorio();
     }
 
     private int generarDestinoAleatorio() {
-        Random rand = new Random();
-        return rand.nextInt(PLANTA_MAX - PLANTA_MIN + 1) + PLANTA_MIN;
+        return random.nextInt(PLANTA_MAX - PLANTA_MIN + 1) + PLANTA_MIN;
+    }
+
+    private int generarTiempoEstanciaAleatorio() {
+        return random.nextInt(MAX_TIEMPO_ESTANCIA - MIN_TIEMPO_ESTANCIA + 1) + MIN_TIEMPO_ESTANCIA;
     }
 
     public int getPlantaDestino() {
-        return destino;
+        return plantaDestino;
     }
 
-    public void setPlantaDestino(int destino) {
-        this.destino = destino;
+    public int getTiempoEstancia() {
+        return tiempoEstancia;
     }
 
+    /**
+     * Decide si llama al ascensor.
+     * @return true si decide llamar, false si opta por no llamar.
+     */
+    public boolean quiereLlamarAscensor() {
+        return random.nextDouble() < PROBABILIDAD_LLAMAR;
+    }
+
+    /**
+     * La propia persona encola en la planta y crea la llamada al ascensor.
+     *
+     * @param control  el controlador de ascensores
+     * @param planta   la planta desde la que llama (debe corresponder al origen)
+     * @param origen   índice de la planta origen (p. ej. Universidad.INGRESO)
+     */
+    public void llamarAscensor(ControlAscensor control, Planta planta, int origen) {
+        planta.personaEsperaAscensor(this);
+        Llamada llamada = new Llamada(origen, this.plantaDestino, this);
+        control.procesarLlamada(llamada);
+    }
+
+    /**
+     * Decrementa el tiempo de estancia en la planta.
+     */
     public void decrementarTiempo() {
-        if (tiempoRestante > 0)
-            tiempoRestante--;
-        if (tiempoRestante <= 0 && destino != PLANTA_INGRESO)
-            quiereSalir = true;
+        if (tiempoEstancia > 0) tiempoEstancia--;
     }
 
+    /**
+     * Indica si la persona debe salir de la planta (ha cumplido su estancia).
+     */
     public boolean debeSalir() {
-        return quiereSalir;
+        return tiempoEstancia <= 0 && !salirDefinitivo;
     }
 
+    /**
+     * Marca que la persona debe salir definitivamente (por ejemplo, al llegar a ingreso y querer irse).
+     */
     public void marcarSalida() {
-        this.quiereSalir = false;
-        this.destino = PLANTA_INGRESO;
-        this.tiempoRestante = 0;
+        salirDefinitivo = true;
+        haSalido = true;
+    }
+        public void setPlantaDestino(int destino) {
+        this.plantaDestino = destino;
     }
 
-    private boolean estaEnIngreso() {
-        return destino == PLANTA_INGRESO;
-    }
-
-    private boolean haCumplidoTiempo() {
-        return tiempoRestante <= 0;
-    }
-
-    private boolean noQuiereSalir() {
-        return !quiereSalir;
-    }
-
+    /**
+     * Indica si la persona ya ha salido definitivamente del edificio.
+     */
     public boolean haSalido() {
-        return estaEnIngreso() && haCumplidoTiempo() && noQuiereSalir();
+        return haSalido;
     }
-
 }
